b5c4796b56716f5b61fd0397aed12866e3901e1c
Ruslan Kostiuk <ruslankostiuk@gmail.com>
2025-05-07 20:46:55 +0300

    added es lint

---
+++ eslint.config.js
@@ -0,0 +1,65 @@
+/* eslint-disable no-undef */
+
+const {FlatCompat} = require('@eslint/eslintrc');
+const angularPlugin = require('@angular-eslint/eslint-plugin');
+const tsPlugin = require('@typescript-eslint/eslint-plugin');
+const tsParser = require('@typescript-eslint/parser');
+const prettierPlugin = require('eslint-plugin-prettier');
+const angularTemplateParser = require('@angular-eslint/template-parser');
+
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+  recommendedConfig: require('@eslint/js').configs.recommended,
+});
+
+
+module.exports = [
+  {
+    files: ['**/*.ts'],
+    ignores: ['**/*.spec.ts', 'eslint.config.js', 'dist/', 'coverage/', 'node_modules/'],
+    languageOptions: {
+      parser: tsParser,
+      parserOptions: {
+        project: ['./tsconfig.json'],
+        sourceType: 'module',
+      },
+      globals: {
+        console: 'readonly',
+        window: 'readonly',
+        document: 'readonly',
+      },
+    },
+    plugins: {
+      '@typescript-eslint': tsPlugin,
+      '@angular-eslint': angularPlugin,
+      prettier: prettierPlugin,
+    },
+    rules: {
+      // Prettier
+      'prettier/prettier': 'warn',
+
+      // Angular ESLint rules
+      '@angular-eslint/directive-selector': [
+        'error',
+        {type: 'attribute', prefix: 'app', style: 'camelCase'},
+      ],
+      '@angular-eslint/component-selector': [
+        'error',
+        {type: 'element', prefix: 'app', style: 'kebab-case'},
+      ],
+    },
+  },
+  {
+    files: ['**/*.html'],
+    languageOptions: {
+      parser: angularTemplateParser,
+    },
+    plugins: {
+      '@angular-eslint': angularPlugin,
+    },
+    rules: {
+      // Можеш додати правила шаблонів тут
+    },
+  },
+  ...compat.extends('eslint:recommended'),
+];
