2e701e1cde73576247daf05d35834d120d60b565
Ruslan Kostiuk <ruslankostiuk@gmail.com>
2025-05-07 21:13:29 +0300

    added login component

---
+++ src/app/domain/models/credentials.model.spec.ts
@@ -0,0 +1,7 @@
+import { CredentialsModel } from './credentials.model';
+
+describe('CredentialsModel', () => {
+  it('should create an instance', () => {
+    expect(new CredentialsModel()).toBeTruthy();
+  });
+});
