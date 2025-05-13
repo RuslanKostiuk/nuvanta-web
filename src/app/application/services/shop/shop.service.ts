b21e97c2c8fd3ca61bae8463b401a4f648e9eae5
Ruslan Kostiuk <ruslankostiuk@gmail.com>
2025-05-13 22:40:42 +0300

    updated switcher

--- src/app/application/services/shop/shop.service.ts
+++ src/app/application/services/shop/shop.service.ts
@@ -1,14 +1,14 @@
-import { inject, Injectable, signal } from '@angular/core';
-import { map, tap } from 'rxjs';
-import { ShopApiService } from '@infrastructure/api';
-import { Shop } from '@domain/models';
-import { ShopMapper } from '@infrastructure/mappers';
+import {inject, Injectable, signal} from '@angular/core';
+import {map, tap} from 'rxjs';
+import {ShopApiService} from '@infrastructure/api';
+import {Shop} from '@domain/models';
+import {ShopMapper} from '@infrastructure/mappers';
 
-@Injectable({ providedIn: 'root' })
+@Injectable({providedIn: 'root'})
 export class ShopService {
   private readonly _api = inject(ShopApiService);
 
-  private readonly _shops = signal<Shop[] | null>(null);
+  private readonly _shops = signal<Shop[]>([]);
 
   readonly shops = this._shops.asReadonly();
 
