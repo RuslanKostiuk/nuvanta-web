9aba51513182eb452d55e153f886479ccf4fa7b9
Ruslan Kostiuk <ruslankostiuk@gmail.com>
2025-05-14 12:46:17 +0300

    added edit product modal

--- src/app/presentation/ui-elements/product-row/product-row.component.ts
+++ src/app/presentation/ui-elements/product-row/product-row.component.ts
@@ -1,18 +1,29 @@
-import {ChangeDetectionStrategy, Component, Input} from '@angular/core';
+import {ChangeDetectionStrategy, Component, input, output, signal} from '@angular/core';
 import {Product} from '@domain/models';
+import {ProductEditModalComponent} from '@presentation/modals/product-edit-modal/product-edit-modal.component';
 
 @Component({
   selector: 'app-product-row',
-  imports: [],
+  imports: [
+    ProductEditModalComponent
+  ],
   templateUrl: './product-row.component.html',
   styleUrl: './product-row.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class ProductRowComponent {
-  @Input() product!: Product;
+  readonly product = input.required<Product>();
+  readonly update = output<Product>();
+
+  isDialogOpen = signal(false);
+
 
   onEdit() {
-    // TODO: open modal or emit event
-    console.log('Product Edit');
+    this.isDialogOpen.set(true);
+  }
+
+  onSave(updated: Product) {
+    this.update.emit(updated);
+    this.isDialogOpen.set(false);
   }
 }
