2ce1e3a8d27b255211250cfa05686c51314cc1a3
Ruslan Kostiuk <ruslankostiuk@gmail.com>
2025-05-15 21:44:47 +0300

    fixed small issues for product images

--- src/app/presentation/ui-elements/product-images/product-images.component.ts
+++ src/app/presentation/ui-elements/product-images/product-images.component.ts
@@ -1,14 +1,19 @@
-import {Component, Input} from '@angular/core';
+import {ChangeDetectorRef, Component, ElementRef, inject, Input, ViewChild} from '@angular/core';
 import {FormArray, FormControl, ReactiveFormsModule} from '@angular/forms';
+import {NgClass} from '@angular/common';
 
 @Component({
   selector: 'app-product-images',
-  imports: [ReactiveFormsModule],
+  imports: [ReactiveFormsModule, NgClass],
   templateUrl: './product-images.component.html',
   styleUrl: './product-images.component.scss'
 })
 export class ProductImagesComponent {
   @Input({required: true}) formArray!: FormArray;
+  @ViewChild('previewListRef') previewListRef!: ElementRef<HTMLDivElement>;
+
+  isDragging = false;
+  private _cdr = inject(ChangeDetectorRef);
 
   onFileSelected(event: Event) {
     const file = (event.target as HTMLInputElement).files?.[0];
@@ -17,17 +22,30 @@ export class ProductImagesComponent {
 
   onDrop(event: DragEvent) {
     event.preventDefault();
+    event.stopPropagation();
+    this.isDragging = false;
+
     const file = event.dataTransfer?.files?.[0];
     if (file) this.readFile(file);
   }
 
   onDragOver(event: DragEvent) {
     event.preventDefault();
+    event.stopPropagation();
+    this.isDragging = true;
+  }
+
+
+  onDragLeave(event: DragEvent) {
+    event.preventDefault();
+    this.isDragging = false;
   }
 
   removeImage(index: number) {
-    debugger;
     this.formArray.removeAt(index);
+    setTimeout(() => {
+      this.previewListRef?.nativeElement.scrollIntoView({behavior: 'smooth', block: 'center'});
+    });
   }
 
   private readFile(file: File) {
@@ -36,6 +54,7 @@ export class ProductImagesComponent {
     reader.onload = () => {
       const url = reader.result as string;
       this.formArray.push(new FormControl(url));
+      this._cdr.markForCheck();
     };
     reader.readAsDataURL(file);
   }
