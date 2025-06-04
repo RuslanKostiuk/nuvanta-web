926e64babfa54ac80d64cad81da48409f1611e47
Ruslan Kostiuk <ruslankostiuk@gmail.com>
2025-06-04 20:57:25 +0300

    added get by id pipe and displayed category in list

--- src/app/presentation/ui-elements/product-row/product-row.component.ts
+++ src/app/presentation/ui-elements/product-row/product-row.component.ts
@@ -1,11 +1,14 @@
-import {ChangeDetectionStrategy, Component, input, signal} from '@angular/core';
+import {ChangeDetectionStrategy, Component, inject, input, signal} from '@angular/core';
 import {ProductPreview} from '@domain/models';
 import {ProductEditModalComponent} from '@presentation/modals/product-edit-modal/product-edit-modal.component';
+import {ProductCategoryService} from '@application/services';
+import {GetByIdPipe} from '@shared/pipes';
 
 @Component({
   selector: 'app-product-row',
   imports: [
-    ProductEditModalComponent
+    ProductEditModalComponent,
+    GetByIdPipe,
   ],
   templateUrl: './product-row.component.html',
   styleUrl: './product-row.component.scss',
@@ -13,9 +16,9 @@ import {ProductEditModalComponent} from '@presentation/modals/product-edit-modal
 })
 export class ProductRowComponent {
   readonly product = input.required<ProductPreview>();
-
   isDialogOpen = signal(false);
-
+  private categoryService = inject(ProductCategoryService);
+  categories = this.categoryService.categories;
 
   onEdit() {
     this.isDialogOpen.set(true);
