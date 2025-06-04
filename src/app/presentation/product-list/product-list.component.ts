583c86443c1c164528d1df93b39f16039f825b42
Ruslan Kostiuk <ruslankostiuk@gmail.com>
2025-06-04 22:06:10 +0300

    added filters and sort select to ui

--- src/app/presentation/product-list/product-list.component.ts
+++ src/app/presentation/product-list/product-list.component.ts
@@ -1,8 +1,9 @@
 import {Component, inject, signal} from '@angular/core';
-import {ProductService} from '@application/services';
+import {ProductCategoryService, ProductService} from '@application/services';
 import {ProductRowComponent} from '@presentation/ui-elements/product-row/product-row.component';
 import {ProductAddModalComponent} from '@presentation/modals/product-add-modal/product-add-modal.component';
 import {TooltipDirective} from '@shared/directives';
+import {FormsModule} from '@angular/forms';
 
 @Component({
   standalone: true,
@@ -10,17 +11,39 @@ import {TooltipDirective} from '@shared/directives';
   imports: [
     ProductRowComponent,
     ProductAddModalComponent,
-    TooltipDirective
+    TooltipDirective,
+    FormsModule
   ],
   templateUrl: './product-list.component.html',
   styleUrl: './product-list.component.scss'
 })
 export class ProductListComponent {
   readonly isDialogOpen = signal(false);
+  filters = {
+    categoryId: '',
+    isActive: '',
+    search: '',
+    priceFrom: '',
+    priceTo: '',
+    sortBy: '',
+  };
   private readonly _productService = inject(ProductService);
   readonly products = this._productService.products;
+  private categoryService = inject(ProductCategoryService);
+  categories = this.categoryService.categories;
 
   openAddModal(): void {
     this.isDialogOpen.set(true);
+  }
+
+  clearFilters() {
+    this.filters = {
+      categoryId: '',
+      isActive: '',
+      search: '',
+      priceFrom: '',
+      priceTo: '',
+      sortBy: '',
+    };
   }
 }
