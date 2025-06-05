7568fa3c2e47f376165b53357251ddb8d893bbe1
Ruslan Kostiuk <ruslankostiuk@gmail.com>
2025-06-05 20:54:39 +0300

    added media pt 2

--- src/app/presentation/layout/sidebar/sidebar.component.ts
+++ src/app/presentation/layout/sidebar/sidebar.component.ts
@@ -1,23 +1,30 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
-import { RouteEnum } from '@shared/enums';
-import { RouterLink, RouterLinkActive } from '@angular/router';
-import { LucideAngularModule } from 'lucide-angular';
-import { TranslatePipe } from '@ngx-translate/core';
+import {ChangeDetectionStrategy, Component, signal} from '@angular/core';
+import {RouteEnum} from '@shared/enums';
+import {RouterLink, RouterLinkActive} from '@angular/router';
+import {LucideAngularModule} from 'lucide-angular';
+import {TranslatePipe} from '@ngx-translate/core';
+import {ClickOutsideDirective} from '@shared/directives';
 
 @Component({
   selector: 'app-sidebar',
-  imports: [RouterLink, RouterLinkActive, LucideAngularModule, TranslatePipe],
+  imports: [RouterLink, RouterLinkActive, LucideAngularModule, TranslatePipe, ClickOutsideDirective],
   templateUrl: './sidebar.component.html',
   styleUrl: './sidebar.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class SidebarComponent {
   readonly items = [
-    { labelKey: 'SIDEBAR.DASHBOARD', icon: 'layout-dashboard', route: '/' + RouteEnum.DASHBOARD },
-    { labelKey: 'SIDEBAR.PRODUCTS', icon: 'package', route: '/' + RouteEnum.PRODUCTS },
-    { labelKey: 'SIDEBAR.CATEGORIES', icon: 'folders', route: '/' + RouteEnum.CATEGORIES },
-    { labelKey: 'SIDEBAR.INVOICES', icon: 'file-text', route: '/' + RouteEnum.INVOICES },
-    { labelKey: 'SIDEBAR.ORDERS', icon: 'receipt', route: '/' + RouteEnum.ORDERS },
-    { labelKey: 'SIDEBAR.USERS', icon: 'users', route: '/' + RouteEnum.USERS },
+    {labelKey: 'SIDEBAR.DASHBOARD', icon: 'layout-dashboard', route: '/' + RouteEnum.DASHBOARD},
+    {labelKey: 'SIDEBAR.PRODUCTS', icon: 'package', route: '/' + RouteEnum.PRODUCTS},
+    {labelKey: 'SIDEBAR.CATEGORIES', icon: 'folders', route: '/' + RouteEnum.CATEGORIES},
+    {labelKey: 'SIDEBAR.INVOICES', icon: 'file-text', route: '/' + RouteEnum.INVOICES},
+    {labelKey: 'SIDEBAR.ORDERS', icon: 'receipt', route: '/' + RouteEnum.ORDERS},
+    {labelKey: 'SIDEBAR.USERS', icon: 'users', route: '/' + RouteEnum.USERS},
   ];
+
+  menuOpen = signal(false);
+
+  toggleMenu() {
+    this.menuOpen.update((x) => !x);
+  }
 }
