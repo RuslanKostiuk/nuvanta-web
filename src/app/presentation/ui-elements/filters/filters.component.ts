86bf22df6d9d18f8c4595b2c7e08b5c577b7778d
Ruslan Kostiuk <ruslankostiuk@gmail.com>
2025-06-05 12:01:19 +0300

    added filters via dynamic component and reactive form

---
+++ src/app/presentation/ui-elements/filters/filters.component.ts
@@ -0,0 +1,32 @@
+import {Component, input, output} from '@angular/core';
+import {FormGroup, ReactiveFormsModule} from '@angular/forms';
+
+export type FilerComponentSettings = {
+  type: 'text' | 'number' | 'select',
+  label?: string;
+  min?: number;
+  max?: number;
+  placeholder?: string;
+  formControlName: string;
+  options?: { id: string | number | boolean | null, name: string }[];
+}
+
+@Component({
+  selector: 'app-filters',
+  templateUrl: './filters.component.html',
+  styleUrl: './filters.component.scss',
+  imports: [
+    ReactiveFormsModule
+  ]
+})
+export class FiltersComponent {
+  formGroup = input.required<FormGroup>()
+
+  clear = output<void>();
+  settings = input.required<FilerComponentSettings[]>()
+
+
+  clearFilters(): void {
+    this.clear.emit();
+  }
+}
