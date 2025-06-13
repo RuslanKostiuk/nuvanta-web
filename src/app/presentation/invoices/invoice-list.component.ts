30e9447b5e456cc4f26400dd8e772c8e791b8436
Ruslan Kostiuk <ruslankostiuk@gmail.com>
2025-06-13 19:44:39 +0300

    added date range filter

--- src/app/presentation/invoices/invoice-list.component.ts
+++ src/app/presentation/invoices/invoice-list.component.ts
@@ -2,13 +2,14 @@ import {Component, inject, OnInit} from '@angular/core';
 import {InvoiceService} from '@application/services';
 import {LucideAngularModule} from 'lucide-angular';
 import {DatePipe, DecimalPipe} from '@angular/common';
+import {NgxDaterangepickerMd} from 'ngx-daterangepicker-material';
 
 @Component({
   selector: 'app-invoice-list',
   templateUrl: './invoice-list.component.html',
   styleUrls: ['./invoice-list.component.scss'],
   standalone: true,
-  imports: [LucideAngularModule, DecimalPipe, DatePipe]
+  imports: [LucideAngularModule, DecimalPipe, DatePipe, NgxDaterangepickerMd]
 })
 export class InvoiceListComponent implements OnInit {
   private _invoiceService = inject(InvoiceService);
